10762a2225fc0a42d2b1f05017a8d4931d00872f
mario872 <106939571+mario872@users.noreply.github.com>
2022-06-17 17:39:56 +1000

    Add files via upload
    Modules and sounds added.

---
+++ TARDIS-CONSOLE/TARDISOUND.py
@@ -0,0 +1,23 @@
+import pygame
+#file setup
+path = 'sounds/'
+sound_files = ["TARDIS-Take-off.mp3", "TARDIS-Land.mp3", "TARDIS-Ambient.mp3"]
+
+#pygame setup
+pygame.mixer.init()
+
+#pygame.play
+def play(string, vol):
+    pygame.mixer.music.set_volume(vol)
+    pygame.mixer.music.load(path + string)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    
+def playNo(string, vol):
+    pygame.mixer.music.set_volume(vol)
+    pygame.mixer.music.load(path + string)
+    pygame.mixer.music.play()
+
+def stop():
+    pygame.mixer.music.stop()
